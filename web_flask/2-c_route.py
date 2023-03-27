fb534c1d52e744ba8f0d2d721ddf77bb0831670b
WoodsonTD <taylordwoodson@gmail.com>
2023-03-27 15:19:00 -0500

    add

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""script that starts a flask web application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """function that displays Hello HBNB"""
+    return("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_1():
+    """function that displays HBNB"""
+    return("HBNB")
+
+
+@app.route('/c/<text>', stict_slashes=False)
+def hbnb_2(text):
+    """function that displays "C" followed by the valuse of the text"""
+    return("C {}".format(text.replace("_", " ")))
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=None)
