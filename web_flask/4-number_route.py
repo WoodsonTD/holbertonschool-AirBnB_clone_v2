b95fe4e0fea708a4caac76e846f4ad1fd35b6b6b
WoodsonTD <taylordwoodson@gmail.com>
2023-03-27 15:48:59 -0500

    add

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+"""Script that starts a Flask web application"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_route():
+    """app displaying Hello"""
+
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_route_v1():
+    """App displaying 2 cmd"""
+
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def hello_route_v2(text):
+    """App displaying 3 cmd"""
+
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def hello_route_v3(text='is cool'):
+    """App displaying 4 cmd"""
+
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def hello_route_v4(n):
+    """App displaying 4 cmd"""
+    return "{} is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
