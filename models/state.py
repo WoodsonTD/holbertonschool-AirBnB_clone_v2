f681d7ba38811b37185bbe676e0f0bc6210f5f5a
WoodsonTD <taylordwoodson@gmail.com>
2023-03-28 17:30:32 -0500

    add

--- models/state.py
+++ models/state.py
@@ -11,3 +11,20 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="delete")
+    "City",
+    backref='state',
+    cascade="all, delete",
+    passive_deletes=True)
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """ Getter instance method """
+            all_cities = storage.all(City)
+            city_list = []
+
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+
+            return city_list
