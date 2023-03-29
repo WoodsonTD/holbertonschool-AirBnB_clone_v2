906768e162b3bd0f975257e1c72754cb47d940b9
WoodsonTD <taylordwoodson@gmail.com>
2023-03-29 10:13:38 -0500

    add

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,29 +1,26 @@
 #!/usr/bin/python3
-
 """Script that starts a Flask web application"""
-
 from flask import Flask, render_template
+
+
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
 def hello_route():
     """app displaying Hello"""
-
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hello_route_v1():
     """App displaying 2 cmd"""
-
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def hello_route_v2(text):
     """App displaying 3 cmd"""
-
     return 'C ' + text.replace('_', ' ')
 
 
@@ -31,7 +28,6 @@ def hello_route_v2(text):
 @app.route("/python/<text>", strict_slashes=False)
 def hello_route_v3(text='is cool'):
     """App displaying 4 cmd"""
-
     return 'Python ' + text.replace('_', ' ')
 
 
