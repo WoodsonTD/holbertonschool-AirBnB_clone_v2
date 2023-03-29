2c2b52d2683aef3fe3911b68477ed79fa049534d
WoodsonTD <taylordwoodson@gmail.com>
2023-03-29 10:17:35 -0500

    add

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,47 +1,42 @@
 #!/usr/bin/python3
-"""Script that starts a Flask web application"""
+"""add a route"""
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello_route():
-    """app displaying Hello"""
+@app.route('/number_template/<int:n>', methods=['GET'], strict_slashes=False)
+def displaytemplate(n):
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def hello_hbnb():
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
-def hello_route_v1():
-    """App displaying 2 cmd"""
+@app.route('/hbnb', methods=['GET'], strict_slashes=False)
+def hbnb():
     return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def hello_route_v2(text):
-    """App displaying 3 cmd"""
-    return 'C ' + text.replace('_', ' ')
+@app.route('/c/<text>', methods=['GET'], strict_slashes=False)
+def displayC(text):
+    return "C " + text.replace("_", " ")
 
 
-@app.route("/python", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def hello_route_v3(text='is cool'):
-    """App displaying 4 cmd"""
-    return 'Python ' + text.replace('_', ' ')
+@app.route('/python', defaults={'text': 'is cool'}, methods=['GET'],
+        strict_slashes=False)
+@app.route('/python/<text>', methods=['GET'], strict_slashes=False)
+def displayPython(text):
+    return "Python " + text.replace("_", " ")
 
 
-@app.route("/number/<int:n>", strict_slashes=False)
-def hello_route_v4(n):
-    """App displaying 4 cmd"""
+@app.route('/number/<int:n>', methods=['GET'], strict_slashes=False)
+def nisnumber(n):
     return "{} is a number".format(n)
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def hello_route_v5(n):
-    """App displaying 6 cmd"""
-    return render_template('5-number.html', number=n)
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
